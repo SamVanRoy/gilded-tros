973607f023d90e1a5799fb8abba4ac57f65b3c0c
svroy5 <sam.vanroy@vdab.be>
2022-11-06 23:25:33 +0100

    add default method to ItemWithQuality interface for increasing quality --> attempt to centralize and reuse increasing logic for all items without touching Item class

--- Java/src/main/java/com/gildedtros/ItemWithQuality.java
+++ Java/src/main/java/com/gildedtros/ItemWithQuality.java
@@ -2,4 +2,10 @@ package com.gildedtros;
 
 public interface ItemWithQuality {
     void updateQuality();
+
+    default void increaseQualityFromItemByOne(Item item) {
+        if (item.quality < 50) {
+            item.quality += 1;
+        }
+    }
 }
