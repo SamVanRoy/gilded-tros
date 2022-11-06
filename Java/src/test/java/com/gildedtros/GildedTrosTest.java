f9cf90795f73df547fabc9af53d0d59e9907058f
svroy5 <sam.vanroy@vdab.be>
2022-11-06 13:13:52 +0100

    [adding tests for existing code] add test for decrementing sellIn value with 1 correctly

--- Java/src/test/java/com/gildedtros/GildedTrosTest.java
+++ Java/src/test/java/com/gildedtros/GildedTrosTest.java
@@ -1,5 +1,34 @@
 package com.gildedtros;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class GildedTrosTest {
 
+    @Nested
+    @DisplayName("Item sellIn decrements correctly")
+    class ItemSellInDecrementsCorrectly {
+
+        @Test
+        void givenAnItemWithASellIn_whenUpdateQuality_thenSellInFromItemDecrementedWith1() {
+            GildedTros app = newGildedTros("Jupke", 10, 40);
+            Item[] expectedItems = createItems("Jupke", 9, 40);
+
+            app.updateQuality();
+
+            assertThat(app.items[0].sellIn).isEqualTo(expectedItems[0].sellIn);
+        }
+    }
+
+    private GildedTros newGildedTros(String itemName, int itemSellIn, int itemQuality) {
+        Item[] item = createItems(itemName, itemSellIn, itemQuality);
+        return new GildedTros(item);
+    }
+
+    private Item[] createItems(String name, int sellIn, int quality) {
+        return new Item[]{new Item(name, sellIn, quality)};
+    }
 }
